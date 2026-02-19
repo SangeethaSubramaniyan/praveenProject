c21d1343175396a6600904c5599b6bc78874cbf6
SangeethaSubramaniyan <sangee.12sm@gmail.com>
2026-02-20 00:25:00 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/PraveenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraveenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraveenprojectApplication.class, args);
+	}
+
+}
